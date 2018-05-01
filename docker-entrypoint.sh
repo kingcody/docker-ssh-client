2bbc64e4a9b3cd072bde8a058b4578df1982cc83
kingcody <me@codymize.com>
2018-04-30 22:51:31 -0400

    add initial setup

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+eval $(ssh-agent -s) > /dev/null
+
+# Add ssh private key
+if [[ ! -z "$SSH_PRIVATE_KEY" ]]; then
+  echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - 2> /dev/null
+fi
+
+# Add known host
+if [[ ! -z "$SSH_KNOWN_HOSTS" ]]; then
+  install -dm 0700 ~/.ssh
+  OLDIFS=$IFS
+  IFS=","
+  for HOST in $SSH_KNOWN_HOSTS; do
+    ssh-keyscan "$HOST" >> ~/.ssh/known_hosts 2> /dev/null
+  done
+  IFS=$OLDIFS
+  chmod 644 ~/.ssh/known_hosts
+fi
+
+# Execute the container command
+exec $@
